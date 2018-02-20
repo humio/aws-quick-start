31762a8ce3e856eb85bb5f8f563358c1db2c9b21
Peter Mechlenborg <peter@humio.com>
2018-02-20 12:10:23 +0100

    Add scripts

---
+++ aws/humio-mount-datadir.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -xe
+
+FSREADY="false"
+
+### Is there a data volume attached to sdh?
+DISK=`file -s /dev/xvdh | grep "No such file or dir" | wc -l`
+if [ "$DISK" == "1" ]; then
+    echo "ERROR: /dev/xvdh (sdh) does not exist! Attach a data volume to the EC2 instance."
+    exit 1
+fi
+
+### Does the data device contain a filesystem?
+DISK=`file -s /dev/xvdh | grep "ext4 filesystem" | wc -l`
+if [ "$DISK" == "1" ] ; then
+    echo "File system exist"
+    FSREADY="true"
+fi
+
+### Does the data device cantain no filesystem?
+DISK=`file -s /dev/xvdh | grep "/dev/xvdh: data" | wc -l`
+if [ "$DISK" == "1" ] ; then
+    echo "No filesystem, formatting disk"
+    mkfs -t ext4 /dev/xvdh
+    FSREADY="true"
+fi
+
+### If we are not ready by now we should abort.
+if [ "$FSREADY" == "false" ] ; then
+    echo "ERROR: Something unexpected happned. Contact support@humio.com"
+    exit 2
+fi
+
+### Mounting the data volume
+echo "Mounting disk at /data"
+mkdir -p /data
+mount /dev/xvdh /data/ || true
+
---
+++ aws/setup-server.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -xe
+
+DIR=`dirname $0`
+
+apt-get update
+apt-get -y install curl
+
+cat << EOF | tee humio-limits.conf
+# Added by humio provisioning script: Raise limits for files.
+* soft nofile 250000
+* hard nofile 250000
+EOF
+cp humio-limits.conf /etc/security/limits.d/humio-limits.conf
+
+cat << EOF | tee 99-humio.conf
+# Allow larger backlog of incoming TCP connections:
+net.core.somaxconn=4096
+net.ipv4.tcp_max_syn_backlog=4096
+# Allow larger buffer of incoming data in particular needed for UDP:
+net.core.rmem_max=16777216
+EOF
+cp 99-humio.conf /etc/sysctl.d/99-humio.conf
+
+
+
+### If we haven't got a humio configuration file we should create a default one matching the about of memory availabe on the instance
+if [ ! -e /etc/humio.conf ]; then
+    MEM=`cat /proc/meminfo | grep MemTotal | sed -r 's/MemTotal: +(.*) kB/\1/'`
+    if   (( MEM  > 16000000 )) ; then   echo "HUMIO_JVM_ARGS=-Xmx8G -Xss2M -XX:MaxDirectMemorySize=32G"   > /etc/humio.conf
+    elif (( MEM  >  8000000 )) ; then   echo "HUMIO_JVM_ARGS=-Xmx4G -Xss2M -XX:MaxDirectMemorySize=32G"   > /etc/humio.conf
+    elif (( MEM  >  4000000 )) ; then   echo "HUMIO_JVM_ARGS=-Xmx2G -Xss2M -XX:MaxDirectMemorySize=32G"   > /etc/humio.conf
+    elif (( MEM  >  2000000 )) ; then   echo "HUMIO_JVM_ARGS=-Xmx1G -Xss2M -XX:MaxDirectMemorySize=32G"   > /etc/humio.conf
+    else
+        echo "ERROR: Not enough memory"
+        exit 3
+    fi
+fi
+
+cp $DIR/humio-mount-datadir.service /etc/systemd/system/
+systemctl enable humio-mount-datadir.service
+systemctl daemon-reload
+systemctl start humio-mount-datadir
+
+#install docker
+if [ ! -f "/usr/bin/docker" ]; then
+    curl -fsSL https://get.docker.com/ | sh
+fi
+
+service docker restart
+
